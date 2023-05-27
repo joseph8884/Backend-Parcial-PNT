a0eccf50a266372aad17630f36c08c170afa1c65
joseph8884 <josegzm08@gmail.com>
2023-05-27 17:11:58 -0500

    add WebConfiguration which solve problems with react

---
+++ src/main/java/USabana/Registro/Practicas/Backend/Parcial/PNT/WebConfiguration.java
@@ -0,0 +1,11 @@
+package USabana.Registro.Practicas.Backend.Parcial.PNT;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedMethods("");
+    }
+}
